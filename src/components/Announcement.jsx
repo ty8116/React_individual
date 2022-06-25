7d5d7d2046d85128f6578830c5de71438fea0d71
최태영 <80656696+ty8116@users.noreply.github.com>
2022-06-25 22:31:56 +0900

    Add files via upload

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: #007ad1;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>맛있는 조각피자를 매점에서?!?!?!?!!?!?!?!?!?!!?!?!?!?!?!?!</Container>;
+};
+
+export default Announcement;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://preview.free3d.com/img/2019/02/2145915189990524359/2nde8l5c-900.jpg",
+      title: "맛있는 조각피자!",
+      desc: "를 매점에서..?",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://news.nateimg.co.kr/orgImg/it/2020/11/26/2020112601531_0.jpg",
+      title: "바나나맛 우유!",
+      desc: "온라인에서 구매 시 5% 할인",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/pd/22/7/3/8/7/7/1/jHeNH/4183738771_B.jpg",
+      title: "포켓몬 빵",
+      desc: "없어서 못구하는 포켓몬 빵을 매점에서~!^^*",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://m.movingpeople.kr/web/product/big/202111/92634362f93d0503746e8c4b26ff209b.jpg",
+      title: "바나나맛 우유",
+    },
+    {
+      id: 2,
+      img: "http://www.thessan.com/shopimages/thessancom/0060020000712.jpg?1486711058",
+      title: "딸기맛 마이쮸",
+    },
+    {
+      id: 3,
+      img: "https://red.lotteon.com/_m2_/_rsc_/LF1005357/?sref=http://image.lottesuper.co.kr/static-root/fileupload/images/8801115134237_S_3%20(7).jpg",
+      title: "초코우유",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://contents.lotteon.com/itemimage/_v071708/LM/88/01/06/23/83/35/1_/00/1/LM8801062383351_001_1.jpg/dims/optimize/dims/resizemc/400x400",
+    },
+    {
+      id:2,
+      img:"https://i.pinimg.com/originals/fa/cd/a3/facda3447183978197a426db945763d1.jpg",
+    },
+    {
+      id:3,
+      img:"https://pbs.twimg.com/media/DSexwR2UQAE8uqu.jpg",
+    },
+    {
+      id:4,
+      img:"https://i.pinimg.com/originals/fa/cd/a3/facda3447183978197a426db945763d1.jpg",
+    },
+    {
+      id:5,
+      img:"https://w.namu.la/s/8dd0c2e59ef24c9718f89f1c609c25a2233054c4506307866862250aa747f498086eeef320f809b40262994009062c008e7d7e26305b5d21d49d6f11463e7d14e528ee9d532361a9cbea5b84120e6e54",
+    },
+    {
+      id:6,
+      img:"https://i.pinimg.com/originals/fa/cd/a3/facda3447183978197a426db945763d1.jpg",
+    },
+    {
+      id:7,
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHNrLQjdQVwCc_fWVeNUM4etRoY3v22RO4bdmyZdR4-4Ly_B6swRY3juxHjgyRnl1fDmk&usqp=CAU",
+    },
+    {
+      id:8,
+      img:"https://i.pinimg.com/originals/fa/cd/a3/facda3447183978197a426db945763d1.jpg",
+    },
+  ]
